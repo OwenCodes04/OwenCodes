ed651157ded253a50c6486f3fcf16bcfae3417c4
OwenCodes04 <149099457+OwenCodes04@users.noreply.github.com>
2024-02-01 11:35:00 -0500

    Fixing shit

--- src/components/Bar.js
+++ src/components/Bar.js
@@ -1,7 +1,8 @@
 const Bar = () => {
     return (
         <div class="navbar bg-base-100">
-        <a class="btn btn-ghost text-xl">Owen Mullins</a>
+        {/* <a class="btn btn-ghost text-xl">Owen Mullins</a> */}
+        <h1 class="btn btn-ghost text-xl">Owen Mullins</h1>
       </div>
     );
   };
